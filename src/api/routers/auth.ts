3c3cb203ab858e8184990fd0103494a89d813f14
Tommy's Macbook Pro(M3 Pro) <zwt03712@gmail.com>
2025-04-19 16:02:52 +1000

    Remove unused authentication route from the API router

--- src/api/routers/auth.ts
+++
@@ -1,9 +0,0 @@
-import express, { type Request, type Response } from "express";
-
-const app = express();
-
-// Root URL
-app.get("/", (req: Request, res: Response) => {
-  console.log("Print to terminal: someone accessed our root url!");
-  res.json({ message: "Welcome to Lab05 Checkins Server's root URL!" });
-});
